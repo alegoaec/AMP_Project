1e546689d56b502860862cd6ab4eae19fae981ff
Stanley <stanley.leckert@edhec.com>
2025-01-16 14:31:34 +0100

    Added AMP.R to the folder

---
+++ AMP.R
@@ -0,0 +1,4 @@
+library(readxl)
+
+data = read_excel("/Users/stanleyleckert/Library/Mobile Documents/com~apple~CloudDocs/EDHEC/M2/S2/Master Project/Datasets/compustat_definitions.xlsx")
+print(data)
